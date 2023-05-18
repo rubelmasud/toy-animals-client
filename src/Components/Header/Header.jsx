8fffc5206ca3839861e14ac2ef3b5ec6f540744e
MD:Rubrl RM <inforubel251@gmail.com>
2023-05-18 16:56:46 +0600

    add to toys gallrey

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className='banner w-full md:h-screen  pl-56 md:pt-28 ' >
-            <div className="w-8/12 mx-auto bg-gradient-to-r from-orange-50 p-12 rounded-lg">
-                <h1 className='text-6xl '>Best Children <span className='text-blue-900 animate-pulse font-bold'> Animals Toys</span> Project !!</h1>
+        <div className='banner w-full md:h-screen  md:pl-56 md:pt-28 ' >
+            <div className="md:w-8/12 mx-auto bg-gradient-to-r from-slate-50 p-12 rounded-lg">
+                <h1 className='md:text-6xl text-3xl'>Best Children <span className='text-blue-900 animate-pulse font-bold'> Animals Toys</span> Project !!</h1>
                 <p className='text-start my-6  text-slate-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ea nam pariatur, voluptate soluta rerum facere deserunt itaque sunt debitis reiciendis veniam eligendi ex et sapiente. Impedit dolores perferendis minima consequuntur. Suscipit, cumque quam dolores dicta temporibus molestias quaerat pariatur.</p>
                 <button className="btn btn-outline btn-secondary animate-bounce justify-end px-8">visit this side</button>
             </div>
--- src/Pages/HomePage/Home/Home.jsx
+++ src/Pages/HomePage/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../../../Components/Header/Header';
+import Gallery from '../../../Components/ToysGallery/Gallery';
+import ShopByCategory from '../../../Components/ShopByCategory/ShopByCategory';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <Gallery></Gallery>
+            <ShopByCategory></ShopByCategory>
         </div>
     );
 };
